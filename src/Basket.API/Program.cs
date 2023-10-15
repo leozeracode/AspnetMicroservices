3411e06f867d6c9e7adab8b72bf86f1a83b057b5
Leonardo Viana <leozeracode@gmail.com>
2023-10-15 16:32:13 -0300

    feat: add controller basket

--- src/Basket.API/Program.cs
+++ src/Basket.API/Program.cs
@@ -1,18 +1,24 @@
+using Basket.API.Repositories;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:RedisConnectionString");
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
 });
 
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
-});
+
 
 var app = builder.Build();
 
