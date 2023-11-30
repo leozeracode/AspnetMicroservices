62c8bcf302a446faa682764f8ff0115b789ea82d
Leonardo Viana <leozeracode@gmail.com>
2023-11-30 19:10:28 -0300

    feat: add behaviors

---
+++ src/Ordering.Application/Behaviours/UnhandledExceptionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return next();
+            }
+            catch (Exception)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError("Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
